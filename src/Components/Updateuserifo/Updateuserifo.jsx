0c4d6a0fedbf05c25a662793703b6d3db8bda37a
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-08-21 21:52:30 -0800

    commit

--- src/Components/Updateuserifo/Updateuserifo.jsx
+++ src/Components/Updateuserifo/Updateuserifo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Firebase from '../Firebase/Firebase'
 import { useParams } from 'react-router';
 import { useEffect,useState } from 'react';
+import profile from '../../images/userprofile.png'
 
 const Updateuserifo = () => {
     const db = Firebase.firestore();
@@ -19,8 +20,24 @@ const Updateuserifo = () => {
 
 
 
-    return ( <div className="Updateuserifo">
-        user: {data.name}
+    return ( 
+    <div className="Updateuserifo">
+        <div className="userCard">
+            <div className="mLablogo">
+                <h3><strong>mLab</strong> user</h3>
+            </div>
+            <div className="userdetails">
+                <div className="profilebx">
+                    <img src={profile} alt="profile" />
+                </div>
+                <div className="userContent">
+                    <p>Names: <strong>{data.name+" "+data.surname}</strong></p>
+                    <p>Age: <strong>{data.age}</strong></p>
+                    <p>Location: <strong>{data.location}</strong></p>
+                    <p>Description: <strong>{data.describe}</strong></p>
+                </div>
+            </div>
+        </div>
     </div> );
 }
  
