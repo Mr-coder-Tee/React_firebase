a1f4e67878b74f814f10c3fda5067f31e353bc11
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-08-22 02:46:22 -0800

    done

--- src/Components/NewsFeed/article.jsx
+++ src/Components/NewsFeed/article.jsx
@@ -8,7 +8,19 @@ const Article = ({article}) => {
     console.log("article=====",article)
     return ( <div className="newsArticle">
         <NewsFeedBanner/>
-        article
+        <div className="articleStructure">
+            <div className="headline">
+                <h1>{article.headline.main}</h1>
+                <h5>{"by "+article.byline.original}{" on "}{article.pub_date}</h5>
+            </div>
+            <div className="intro">
+                <h4>{article.abstract}</h4>
+                <p>{article.lead_paragraph}</p>
+                <p>{article.snippet}</p>
+                <p>{article.print_section}</p>
+                <p>{article.web_url}</p>
+            </div>
+        </div>
     </div> );
 }
  
